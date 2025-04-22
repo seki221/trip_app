09bf9b9603049560aa5a338e98454f2c01be9bc4
seki221 <pawls00.bugaboo@icloud.com>
2025-04-22 20:01:49 +0900

    timezoneをTOKYOに変更

--- config/application.rb
+++ config/application.rb
@@ -5,6 +5,7 @@ require "rails/all"
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
+Dotenv.load(".env", ".env.#{Rails.env}") if defined?(Dotenv)
 
 module TripApp
   class Application < Rails::Application
@@ -16,12 +17,13 @@ module TripApp
     # Common ones are `templates`, `generators`, or `middleware`, for example.
     config.autoload_lib(ignore: %w[assets tasks])
 
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
     #
-    # config.time_zone = "Central Time (US & Canada)"
+    config.time_zone = "Tokyo"
     # config.eager_load_paths << Rails.root.join("extras")
   end
 end
