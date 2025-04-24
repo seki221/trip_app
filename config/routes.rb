f767776bd43639454cfda35f31cedc730625e93a
seki221 <pawls00.bugaboo@icloud.com>
2025-04-24 21:21:12 +0900

    add route_planner

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
+  resources :planners
   get "up" => "rails/health#show", as: :rails_health_check
   # Render dynamic PWA files from app/views/pwa/*
   get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
