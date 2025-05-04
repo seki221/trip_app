0f707a64f47af74ab9b76449fdd1f6aa4984c895
seki221 <pawls00.bugaboo@icloud.com>
2025-05-05 06:58:54 +0900

    add sinout

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users
+  # davise関係
+  # devise_for :users, controllers: {
+  #   # omniauth_callbacks: "users/omniauth_callbacks"
+  #   # registrations: 'users/registrations',
+  #   # sessions: 'users/sessions',
+  # }
+  devise_scope :user do
+    get "/users/sign_out" => "devise/sessions#destroy"
+  end
   resources :planners
   get "up" => "rails/health#show", as: :rails_health_check
   # Render dynamic PWA files from app/views/pwa/*
