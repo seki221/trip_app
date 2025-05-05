fb7b4efea686f41db577a4356ca2ea45ac6cb787
seki221 <pawls00.bugaboo@icloud.com>
2025-05-05 21:52:51 +0900

    route planners

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root to: "home#index"
+
   devise_for :users
   # davise関係
   # devise_for :users, controllers: {
@@ -9,13 +11,11 @@ Rails.application.routes.draw do
   devise_scope :user do
     get "/users/sign_out" => "devise/sessions#destroy"
   end
-  resources :planners
-  get "up" => "rails/health#show", as: :rails_health_check
-  # Render dynamic PWA files from app/views/pwa/*
-  get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
-  get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
+  resources :planners, only: %i[index new create show edit update destroy] do
+  end
 
-  # Defines the root path route ("/")
-  # root "posts#index"
-  root to: "home#index"
+  # get "up" => "rails/health#show", as: :rails_health_check
+  # Render dynamic PWA files from app/views/pwa/*
+  # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
+  # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
 end
