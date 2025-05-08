0ec476dcb1c10fdd448473a35637ca601cc3b424
seki221 <pawls00.bugaboo@icloud.com>
2025-05-08 23:57:56 +0900

    add routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,16 +2,12 @@ Rails.application.routes.draw do
   root to: "home#index"
 
   devise_for :users
-  # davise関係
-  # devise_for :users, controllers: {
-  #   # omniauth_callbacks: "users/omniauth_callbacks"
-  #   # registrations: 'users/registrations',
-  #   # sessions: 'users/sessions',
-  # }
-  devise_scope :user do
-    get "/users/sign_out" => "devise/sessions#destroy"
-  end
-  resources :planners, only: %i[index new create show edit update destroy] do
+
+    devise_scope :user do
+      get "/users/sign_out" => "devise/sessions#destroy"
+    end
+    resources :planners, only: %i[index new create show edit update destroy] do
+      resources :schedules
   end
 
   # get "up" => "rails/health#show", as: :rails_health_check
@@ -19,3 +15,9 @@ Rails.application.routes.draw do
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
   # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
 end
+# davise関係
+  # devise_for :users, controllers: {
+    #   # omniauth_callbacks: "users/omniauth_callbacks"
+    #   # registrations: 'users/registrations',
+    #   # sessions: 'users/sessions',
+    # }
