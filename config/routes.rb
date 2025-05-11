ce62310afb42123a157b542d7fae4670a1d17cec
seki221 <pawls00.bugaboo@icloud.com>
2025-05-11 13:22:42 +0900

    add  routes

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
       get "/users/sign_out" => "devise/sessions#destroy"
     end
     resources :planners, only: %i[index new create show edit update destroy] do
-      resources :schedules
+      resources :schedules, only: %i[index new create show edit update destroy]
   end
 
   # get "up" => "rails/health#show", as: :rails_health_check
@@ -15,9 +15,3 @@ Rails.application.routes.draw do
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
   # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
 end
-# davise関係
-  # devise_for :users, controllers: {
-    #   # omniauth_callbacks: "users/omniauth_callbacks"
-    #   # registrations: 'users/registrations',
-    #   # sessions: 'users/sessions',
-    # }
