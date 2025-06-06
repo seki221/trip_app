759a5dd41023dbf02641db7790706f09f30bd8fa
seki221 <pawls00.bugaboo@icloud.com>
2025-06-06 23:45:03 +0900

    add planner validation

--- app/models/planner.rb
+++ app/models/planner.rb
@@ -3,4 +3,8 @@ class Planner < ApplicationRecord
 
   has_many :schedules, dependent: :destroy
   has_many :bookmarks, as: :bookmarkable, dependent: :destroy
+
+  validates :title, presence: true
+  validates :start_date, presence: true
+  validates :end_date, presence: true
 end
