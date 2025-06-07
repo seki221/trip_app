c7df268fc6ef4945ce8a7e9c67836d7793fda1cc
seki221 <pawls00.bugaboo@icloud.com>
2025-06-07 16:53:39 +0900

    [add]bookmarkセレクター

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -2,11 +2,12 @@ class BookmarksController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @bookmarks = current_user.bookmarks.where(bookmarkable_type: "Planner")
+    bookmarks = current_user.bookmarks.includes(:bookmarkable)
 
-    if params[:type_filter].present?
-      @bookmarks = @bookmarks.where(bookmarkable_type: params[:type_filter])
-    end
+    @planners  = bookmarks.select { |b| b.bookmarkable_type == "Planner" }
+    @schedules = bookmarks.select { |b| b.bookmarkable_type == "Schedule" }
+
+    @type_filter = params[:type_filter]
   end
 
   def create
@@ -20,25 +21,6 @@ class BookmarksController < ApplicationController
     end
   end
 
-  def create_schedule
-    bookmark = current_user.bookmarks.find(params[:id])
-    planner_id = params[:planner_id]
-
-    # Bookmark先がScheduleであることを前提に処理（要調整）
-    if bookmark.bookmarkable_type == "Schedule"
-      original = bookmark.bookmarkable
-      Schedule.create!(
-        planner_id: planner_id,
-        destination: original.destination,
-        start_date: original.start_date,
-        description: original.description,
-        # 他にも必要なフィールドをコピー
-      )
-      redirect_to bookmarks_path, notice: "スケジュールを追加しました"
-    else
-      redirect_to bookmarks_path, alert: "対応していないタイプです"
-    end
-  end
 
   def destroy
     @bookmark = Bookmark.find(params[:id])
