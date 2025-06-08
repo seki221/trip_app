d848a3dd46236e57b1badef407fc3667baa5089b
seki221 <pawls00.bugaboo@icloud.com>
2025-06-08 22:46:21 +0900

    [add]test用編集

--- app/models/reschedule.rb
+++ app/models/reschedule.rb
@@ -1,5 +1,5 @@
 class Reschedule < ApplicationRecord
   belongs_to :user
   belongs_to :planner
-  belongs_to :original_schedule, class_name: 'Schedule'
+  belongs_to :original_schedule, class_name: "Schedule"
 end
