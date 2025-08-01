762e815aa278e7909b62b8dd0896aaa6f79462b0
seki221 <pawls00.bugaboo@icloud.com>
2025-08-01 11:34:00 +0900

    add 重複している箇所の削除

--- app/controllers/schedules_controller.rb
+++ app/controllers/schedules_controller.rb
@@ -38,7 +38,6 @@ class SchedulesController < ApplicationController
 
   # POST /schedules or /schedules.json
   def create
-    @schedule = Schedule.new(schedule_params)
     @planner = Planner.find(params[:planner_id])
     @schedule = @planner.schedules.build(schedule_params)
     @schedule.user = current_user
