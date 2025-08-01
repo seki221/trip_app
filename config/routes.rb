83e5e63ce3e490c595c92bec9b1396e7d76c0c08
seki221 <pawls00.bugaboo@icloud.com>
2025-08-01 11:37:50 +0900

    route 微変更

--- config/routes.rb
+++ config/routes.rb
@@ -9,18 +9,28 @@ Rails.application.routes.draw do
   get "sitelists", to: "sitelists#index"
   get "mypages", to: "mypages#index"
 
-  resources :bookmarks, only: [ :index, :create, :destroy ] do
+  resources :bookmarks do
     post :create_schedule, on: :member
   end
-
-  resources :reschedules, only: [ :create ]
-
-
   resources :planners, only: %i[index new create edit update destroy] do
-    resources :schedules, only: %i[index new create show edit update destroy]
+  resources :schedules, only: %i[index new create show edit update destroy]
   end
+
 end
 # get "up" => "rails/health#show", as: :rails_health_check
 # Render dynamic PWA files from app/views/pwa/*
 # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
 # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
+
+
+Rails.application.routes.draw do
+  # planページ
+  resources :planners, only: %i[index new create show edit update destroy] do
+    resources :schedules, only: %i[index new create show edit update destroy] do
+      member do
+        get :bookmarks
+      end
+    end
+  end
+  resources :bookmarks, only: %i[index destroy]
+end
